b4785f7771ead4a32b83eb756387318171a9e5bd
cesarsan13 <cesaromarsancheztapia@gmail.com>
2023-01-23 22:15:26 -0600

    creacion de usercontroller y correccion en seeders

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/login', [UserController::class,'login']);
+Route::post('/signup', [UserController::class,'signup']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
